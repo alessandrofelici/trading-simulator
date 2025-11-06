1239e0cd5cdfb5cc26fbdbb763497339249f4a38
Alex Felici <sandroms521@gmail.com>
2025-11-05 22:12:56 -0500

    rm codio extras

--- .guides/access.sh
+++
@@ -1,4 +0,0 @@
-# Please ignore this file, it is only intended to be used by instructors.
-mv .guides/results.tar.gpg /home/codio/workspace/
-echo $1 | gpg --batch --yes --quiet --passphrase-fd 0 /home/codio/workspace/results.tar.gpg 
-tar -xf /home/codio/workspace/results.tar
